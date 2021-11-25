833d07db40b42635b4169c3e869f614a62d946c1
PFZM <pfzm@hotmail.com>
2021-11-25 18:43:14 +1100

    added index.js - models

--- models/index.js
+++ models/index.js
@@ -2,4 +2,21 @@ const User = require("./User");
 const Aircraft = require("./Aircraft");
 const LogData = require("./LogData");
 
+User.hasMany(LogData, {
+  foreignKey: "pilot_id",
+  onDelete: "CASCADE",
+});
+
+LogData.belongsTo(User, {
+  foreignKey: "pilot-id",
+});
+
+LogData.hasMany(Aircraft, {
+  foreignKey: "aircraft_id",
+});
+
+Aircraft.belongsTo(LogData, {
+  foreignKey: "aircraft_id",
+});
+
 module.exports = { User, Aircraft, LogData };
