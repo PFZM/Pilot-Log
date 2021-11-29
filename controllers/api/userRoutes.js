4ea7a7233e90c1d1cebb785b40fd1af5285c6b38
PFZM <pfzm@hotmail.com>
2021-11-29 13:10:35 +1100

    update userRoute

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "error" });
+  }
+});
+
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -23,7 +39,8 @@ router.post("/login", async (req, res) => {
       res.json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
-    res.status(500).json(err);
+    console.error(err);
+    res.status(500).json({ message: "error" });
   }
 });
 
@@ -36,3 +53,5 @@ router.post("/logout", (req, res) => {
     res.status(404).end();
   }
 });
+
+module.exports = router;
---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // insert date with specific pkg
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+};
