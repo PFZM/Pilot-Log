203361a337ae55c6adb20e3901eba2d0e49fb7fc
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-29 19:27:35 +1100

    added houmerout for newlog

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,32 @@
 const router = require("express").Router();
-const { User, LogData } = require("../models");
+const { User, LogData, Aircraft } = require("../models");
 const withAuth = require("../utils/auth");
 
+router.get("/addNew", withAuth, async (req, res) => {
+  try {
+    const aircraftData = await Aircraft.findAll();
+    const aircrafts = aircraftData.map((aircraft) =>
+      aircraft.get({ aircraft: true })
+    );
+    res.render("logData", {
+      aircrafts,
+      logged_in: req.session.logged_in,
+    });
+    //res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get("/login", (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+    res.render("login");
+  });
+  
+  
 module.exports = router;
