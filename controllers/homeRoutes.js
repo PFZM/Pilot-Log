fe50becdd4bda56360905b6a89d705c986ee6c02
PFZM <pfzm@hotmail.com>
2021-11-29 20:52:07 +1100

    route get. logData:ID

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,15 +18,39 @@ router.get("/addNew", withAuth, async (req, res) => {
   }
 });
 
+// Get LogData by specific ID
+router.get("/logData/:id", withAuth, async (req, res) => {
+  try {
+    const logDataID = await LogData.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ["password"] },
+        },
+        {
+          model: Aircraft,
+        },
+      ],
+    });
+
+    const logData = logDataID.get({ plain: true });
+
+    res.render("logData", {
+      ...logData,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/login", (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect("/");
-      return;
-    }
-    res.render("login");
-  });
-  
-  
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
 module.exports = router;
