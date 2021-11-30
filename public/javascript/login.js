a6cda5251b6d3a629f44afb47d115a20f35a31e5
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-30 20:51:00 +1100

    edited login routes and dashboard routes

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,23 +1,22 @@
-const login =async(event) => {
-    event.preventDefault();
-    const email = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
+const login = async (event) => {
+  event.preventDefault();
+  const email = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-    console.log(email, password)
-
-    if (username && password) {
-        const reponse =  await fetch("/api/users/login", {
-            method: 'post',
-            body: JSON.stringify({email, password}),
-            headers: {"Content-Type": "applications.json"}
-        });
-        if (reponse.ok) {
-            document.location.replace('/dashboard');
-        }
-        else {
-            alert(response.statusText);
-        }
+  if (email && password) {
+    const reponse = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (reponse.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
     }
+  }
 };
 
-document.getElementById("loginButton").addEventListener("submit", login)
+
+
+document.getElementById("loginButton").addEventListener("click", login);
