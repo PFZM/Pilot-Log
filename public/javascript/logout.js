3e838919f3852c35b2f900559ffa55050ca95734
PFZM <pfzm@hotmail.com>
2021-12-02 20:35:40 +1100

    logOut button and add new flight button

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,14 +1,18 @@
-const logout  =(event) => {
-    event.preventDefault();
-    fetch('api/users/logout', {
-        method: "DELETE",
-        headers: {"Content-Type": "application/json"}
-    })
-   if (Response.ok) {
-       document.location.replace("/")
-   } else {
-        alert(response.statusText);
-   };
+const logout = async () => {
+  try {
+    const response = await fetch("api/users/logout", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error loging out" });
+  }
 };
 
-document.getElementById("logout").addEventListener("click",logout);
+document.getElementById("logout").addEventListener("click", logout);
